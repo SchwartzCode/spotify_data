9c853815e42f705d6e310718edd0fa375753c83b
SchwartzCode <jonbs98@gmail.com>
2022-03-25 02:18:21 -0400

    added song play count to StreamingHistory()

--- song_data.py
+++ song_data.py
@@ -1,13 +1,14 @@
 import json
-import string
+from tabulate import tabulate
 
 class Library():
     
-    def __init__(self, path: str):
+    def __init__(self, path: str, max_print_amount=50):
         print(f'Loading data from: {path}')
 
         f = open(path)
         self.lib = json.load(f)
+        self.max_print_amount = max_print_amount
         
     def load_song_data_all_playlists(self):
         playlists = self.lib['playlists']
@@ -40,36 +41,36 @@ class Library():
         
         sorted_songs = sorted(self.playlist_data['songs'].items(), key=lambda x: x[1], reverse=True)
         print(f"{len(self.playlist_data['songs'].keys())} unique songs in library")
-        print('Top Songs:\t[number of appearances]')
-        
-        for i, item in enumerate(sorted_songs):
-            print(f"{i}\t {item[0]} \t\t {item[1]} ")
-        
-            if i > 50:
-                break
+        song_print_col_headers = ["Rank", "Song", "# of Playlist Appearances"]
+        self.print_dict_data(sorted_songs, song_print_col_headers)
 
-        
         sorted_artists = sorted(self.playlist_data['artists'].items(), key=lambda x: x[1], reverse=True)
         print(f"\n\n{len(self.playlist_data['artists'].keys())} unique artists in library")
-        print('Top Artists: \t[number of songs in playlists]')
+        artist_print_col_headers = ["Rank", "Artist", "# of Songs in Playlists"]
+        self.print_dict_data(sorted_artists, artist_print_col_headers)
+
+
+    def print_dict_data(self, dict_in, column_headers):
         
-        for i, item in enumerate(sorted_artists):
-            print(f"{i}\t {item[0]} \t\t {item[1]}")
+        song_info_to_print = []        
         
-            if i > 50:
+        for i, item in enumerate(dict_in):
+            song_info_to_print.append([i+1, item[0], item[1]])
+            
+            if i >= self.max_print_amount-1:
                 break
+            
+        print(tabulate(song_info_to_print, headers=column_headers))
 
-        
-
-        
   
 class StreamingHistory():
     
-    def __init__(self, path: str):
+    def __init__(self, path: str, max_print_amount=50):
         print(f'Loading data from: {path}')
 
         f = open(path)
         self.hist = json.load(f)
+        self.max_print_amount = max_print_amount
         
     def load_streaming_history(self):
         
@@ -80,16 +81,27 @@ class StreamingHistory():
             time_played = elem["msPlayed"] / 3600000 # convert ms to hours
             
             if song_name in self.streaming_data.keys():
-                self.streaming_data[song_name] += time_played
+                self.streaming_data[song_name]['time_played'] += time_played
+                self.streaming_data[song_name]['plays'] += 1
             else:
-                self.streaming_data[song_name] = time_played
+                self.streaming_data[song_name] = {'time_played': time_played, 'plays': 1}
         
     def print_streaming_history(self):
-        sorted_songs = sorted(self.streaming_data.items(), key=lambda x: x[1], reverse=True)
-        print(len(self.streaming_data.keys()))
+        sorted_songs = sorted(self.streaming_data.items(), key=lambda x: x[1]['plays'], reverse=True)
+        print(f"{len(self.streaming_data.keys())} unique songs listen to. Top {self.max_print_amount}:")
         
-        for i, item in enumerate(sorted_songs):
-            print(f"{i}\t {item[0]} \t\t {item[1]:.2f} [hours]")
+        print_col_headers = ['Rank', 'Song Name', 'Plays', 'Hours Listened']
         
-            if i > 50:
+        self.print_dict_data(sorted_songs, print_col_headers)
+        
+    def print_dict_data(self, dict_in, column_headers):
+        
+        song_info_to_print = []        
+        
+        for i, item in enumerate(dict_in):
+            song_info_to_print.append([i+1, item[0], item[1]['plays'], item[1]['time_played']])
+            
+            if i >= self.max_print_amount-1:
                 break
+            
+        print(tabulate(song_info_to_print, headers=column_headers))
